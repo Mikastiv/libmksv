634976814d2ca79372280ba6c1c6b3fad6edbc66
Mikastiv <mikastiv@outlook.com>
2023-09-15 01:18:49 -0400

    vectors

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ OBJS		=	$(SRCS_BASE:%=$(OBJ_DIR)/%.o)
 VPATH		=	$(SRC_DIR) $(DIRS)
 
 CXX			=	clang++
-CXXFLAGS	=	-Wall -Werror -Wextra -Wpedantic -Wfloat-conversion -Wsign-conversion -std=c++20
+CXXFLAGS	=	-Wall -Werror -Wextra -Wpedantic -Wfloat-conversion -Wsign-conversion -Wno-gnu-anonymous-struct -fno-rtti -std=c++20
 AR			=	ar rcs
 RM			=	rm -rf
 MKDIR		=	mkdir -p
--- include/mksv/types.hpp
+++ include/mksv/types.hpp
@@ -15,3 +15,156 @@ typedef int64_t i64;
 
 typedef float f32;
 typedef double f64;
+
+constexpr f32 PI = 3.14159265358979323846264338327950288419716939937510f;
+constexpr f32 TAU = 2.0f * PI;
+
+template <typename T>
+union Vec2 {
+    struct {
+        T x;
+        T y;
+    };
+    T e[2];
+};
+
+template <typename T>
+union Vec3 {
+    struct {
+        T x;
+        T y;
+        T z;
+    };
+    T e[3];
+};
+
+template <typename T>
+union Vec4 {
+    struct {
+        T x;
+        T y;
+        T z;
+        T w;
+    };
+    T e[4];
+};
+
+template <typename T>
+Vec2<T>
+operator+(const Vec2<T>& a, const Vec2<T>& b) {
+    return {
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+    };
+}
+
+template <typename T>
+Vec3<T>
+operator+(const Vec3<T>& a, const Vec3<T>& b) {
+    return {
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+        .z = a.z + b.z,
+    };
+}
+
+template <typename T>
+Vec4<T>
+operator+(const Vec4<T>& a, const Vec4<T>& b) {
+    return {
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+        .z = a.z + b.z,
+        .w = a.w + b.w,
+    };
+}
+
+template <typename T>
+Vec2<T>
+operator-(const Vec2<T>& a, const Vec2<T>& b) {
+    return {
+        .x = a.x - b.x,
+        .y = a.y - b.y,
+    };
+}
+
+template <typename T>
+Vec3<T>
+operator-(const Vec3<T>& a, const Vec3<T>& b) {
+    return {
+        .x = a.x - b.x,
+        .y = a.y - b.y,
+        .z = a.z - b.z,
+    };
+}
+
+template <typename T>
+Vec4<T>
+operator-(const Vec4<T>& a, const Vec4<T>& b) {
+    return {
+        .x = a.x - b.x,
+        .y = a.y - b.y,
+        .z = a.z - b.z,
+        .w = a.w - b.w,
+    };
+}
+
+template <typename T>
+Vec2<T>
+operator*(const Vec2<T>& a, const T& b) {
+    return {
+        .x = a.x * b,
+        .y = a.y * b,
+    };
+}
+
+template <typename T>
+Vec3<T>
+operator*(const Vec3<T>& a, const T& b) {
+    return {
+        .x = a.x * b,
+        .y = a.y * b,
+        .z = a.z * b,
+    };
+}
+
+template <typename T>
+Vec4<T>
+operator*(const Vec4<T>& a, const T& b) {
+    return {
+        .x = a.x * b,
+        .y = a.y * b,
+        .z = a.z * b,
+        .w = a.w * b,
+    };
+}
+
+template <typename T>
+Vec2<T>
+operator/(const Vec2<T>& a, const T& b) {
+    return {
+        .x = a.x / b,
+        .y = a.y / b,
+    };
+}
+
+template <typename T>
+Vec3<T>
+operator/(const Vec3<T>& a, const T& b) {
+    return {
+        .x = a.x / b,
+        .y = a.y / b,
+        .z = a.z / b,
+    };
+}
+
+template <typename T>
+Vec4<T>
+operator/(const Vec4<T>& a, const T& b) {
+    return {
+        .x = a.x / b,
+        .y = a.y / b,
+        .z = a.z / b,
+        .w = a.w / b,
+    };
+}
