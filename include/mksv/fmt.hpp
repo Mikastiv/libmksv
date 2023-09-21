21aeb29d2c550a9032441c45489d05b5fc75fad4
mleblanc <mleblanc@student.42quebec.com>
2023-09-21 14:00:13 -0400

    integer parsing

--- include/mksv/fmt.hpp
+++ include/mksv/fmt.hpp
@@ -15,37 +15,37 @@ parse_float(const Str str, f32* out);
 [[nodiscard]] bool
 parse_float(const Str str, f64* out);
 
-// Integer parsing. Only accepts base 10
+// Integer parsing without overflow checks
 [[nodiscard]] bool
-parse_int(const Str str, u8* out);
+parse_int(const Str str, const u8 base, u8* out);
 
-// Integer parsing. Only accepts base 10
+// Integer parsing without overflow checks
 [[nodiscard]] bool
-parse_int(const Str str, u16* out);
+parse_int(const Str str, const u16 base, u16* out);
 
-// Integer parsing. Only accepts base 10
+// Integer parsing without overflow checks
 [[nodiscard]] bool
-parse_int(const Str str, u32* out);
+parse_int(const Str str, const u32 base, u32* out);
 
-// Integer parsing. Only accepts base 10
+// Integer parsing without overflow checks
 [[nodiscard]] bool
-parse_int(const Str str, u64* out);
+parse_int(const Str str, const u64 base, u64* out);
 
-// Integer parsing. Only accepts base 10
+// Integer parsing without overflow checks
 [[nodiscard]] bool
-parse_int(const Str str, i8* out);
+parse_int(const Str str, const i8 base, i8* out);
 
-// Integer parsing. Only accepts base 10
+// Integer parsing without overflow checks
 [[nodiscard]] bool
-parse_int(const Str str, i16* out);
+parse_int(const Str str, const i16 base, i16* out);
 
-// Integer parsing. Only accepts base 10
+// Integer parsing without overflow checks
 [[nodiscard]] bool
-parse_int(const Str str, i32* out);
+parse_int(const Str str, const i32 base, i32* out);
 
-// Integer parsing. Only accepts base 10
+// Integer parsing without overflow checks
 [[nodiscard]] bool
-parse_int(const Str str, i64* out);
+parse_int(const Str str, const i64 base, i64* out);
 
 } // namespace fmt
 
--- src/fmt.cpp
+++ src/fmt.cpp
@@ -24,11 +24,11 @@ static u64 pow10_table[] = {
     1'000'000'000'000'000,
 };
 
-static u64
-parse_digits(const Str str, u64* idx) {
+static constexpr u64
+parse_digits(const Str str, u64* idx, const u64 base) {
     u64 number = 0;
     while (*idx < str.len && is_digit(str.ptr[*idx])) {
-        number *= 10;
+        number *= base;
         number += str.ptr[*idx] - '0';
         ++(*idx);
     }
@@ -36,7 +36,7 @@ parse_digits(const Str str, u64* idx) {
 }
 
 template <typename T>
-bool
+constexpr bool
 convert_float(const Str str, T* out) {
     if (str.len == 0) return false;
 
@@ -46,12 +46,12 @@ convert_float(const Str str, T* out) {
 
     u64 n_digits = 0;
     const u64 integer_start = idx;
-    const u64 integer = parse_digits(str, &idx);
+    const u64 integer = parse_digits(str, &idx, 10);
     n_digits += idx - integer_start;
 
     if (idx < str.len && str.ptr[idx] == '.') ++idx;
     const u64 decimal_start = idx;
-    const u64 decimal = parse_digits(str, &idx);
+    const u64 decimal = parse_digits(str, &idx, 10);
     const u64 exponent = idx - decimal_start;
     n_digits += exponent;
 
@@ -75,6 +75,83 @@ parse_float(const Str str, f32* out) {
 bool
 parse_float(const Str str, f64* out) {
     return convert_float(str, out);
+}
+
+template <typename T>
+constexpr bool
+convert_uint(const Str str, const T base, T* out) {
+    if (str.len == 0) return false;
+
+    u64 idx = 0;
+    if (str.ptr[idx] == '-') return false;
+
+    if (str.ptr[idx] == '+') ++idx;
+
+    const u64 number = parse_digits(str, &idx, base);
+    *out = (T)number;
+
+    return true;
+}
+
+template <typename T>
+constexpr bool
+convert_int(const Str str, const T base, T* out) {
+    if (str.len == 0) return false;
+
+    u64 idx = 0;
+    const T sign = str.ptr[idx] == '-' ? -1 : 1;
+    if (str.ptr[idx] == '-' || str.ptr[idx] == '+') ++idx;
+
+    T number = 0;
+    while (is_digit(str.ptr[idx])) {
+        number *= base;
+        number += (str.ptr[idx] - '0') * sign;
+        ++idx;
+    }
+
+    *out = number;
+
+    return true;
+}
+
+bool
+parse_int(const Str str, const u8 base, u8* out) {
+    return convert_uint(str, base, out);
+}
+
+bool
+parse_int(const Str str, const u16 base, u16* out) {
+    return convert_uint(str, base, out);
+}
+
+bool
+parse_int(const Str str, const u32 base, u32* out) {
+    return convert_uint(str, base, out);
+}
+
+bool
+parse_int(const Str str, const u64 base, u64* out) {
+    return convert_uint(str, base, out);
+}
+
+bool
+parse_int(const Str str, const i8 base, i8* out) {
+    return convert_int(str, base, out);
+}
+
+bool
+parse_int(const Str str, const i16 base, i16* out) {
+    return convert_int(str, base, out);
+}
+
+bool
+parse_int(const Str str, const i32 base, i32* out) {
+    return convert_int(str, base, out);
+}
+
+bool
+parse_int(const Str str, const i64 base, i64* out) {
+    return convert_int(str, base, out);
 }
 
 } // namespace fmt
