1f9100b1d45ff65aba11142d1f9d2785d0fd3f9d
Mikastiv <mikastiv@outlook.com>
2023-09-26 01:51:27 -0400

    bug fix

--- src/mem.cpp
+++ src/mem.cpp
@@ -9,6 +9,6 @@ bool
 dupe_z(const mksv::mem::Allocator allocator, const Str string, Str* out_string) {
     if (!allocator.alloc(string.len + 1, out_string)) return false;
     mksv::mem::copy({ .ptr = out_string->ptr, .len = string.len }, string);
-    out_string->ptr[out_string->len] = 0;
+    out_string->ptr[string.len] = 0;
     return true;
 }
