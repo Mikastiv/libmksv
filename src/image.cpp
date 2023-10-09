2dc869ca532c1139b17cbe8eca4f88ba4d96c2ff
Mikastiv <mikastiv@outlook.com>
2023-10-08 20:28:54 -0400

    bmp loading progress

--- src/image.cpp
+++ src/image.cpp
@@ -2,6 +2,8 @@
 
 #include "io.hpp"
 
+#include "fmt.hpp"
+
 namespace mksv {
 namespace img {
 
@@ -42,7 +44,7 @@ load_tga(const mem::Allocator allocator, const Str filename, Image* out_image) {
     mem::Slice<u8> image_data = {};
     if (!io::read_file(allocator, filename, &image_data)) return false;
 
-    const TGAHeader* header = (TGAHeader*)&image_data.ptr;
+    const TGAHeader* header = (TGAHeader*)image_data.ptr;
     const bool has_color_map = header->color_map_type;
 
     const TGAImageType type = (TGAImageType)(header->image_type & 0b11);
@@ -57,16 +59,13 @@ load_tga(const mem::Allocator allocator, const Str filename, Image* out_image) {
 }
 
 enum struct BMPCompressionType {
-    RGB = 0,
-    RLE8 = 1,
-    RLE4 = 2,
-    BITFIELDS = 3,
-    JPEG = 4,
-    PNG = 5,
-    ALPHABITFIELDS = 6,
-    CMYK = 11,
-    CMYKRLE8 = 12,
-    CMYKRLE4 = 13,
+    Rgb = 0,
+    Rle8 = 1,
+    Rle4 = 2,
+    Bitfields = 3,
+    Jpeg = 4,
+    Png = 5,
+    AlphaBitfields = 6,
 };
 
 #pragma pack(push)
@@ -87,22 +86,89 @@ struct DIBHeader {
     u16 bpp;
     BMPCompressionType compression;
     u32 image_size;
-    i32 h_resolution;
-    i32 v_resolution;
+    i32 x_pixels_per_meter;
+    i32 y_pixels_per_meter;
     u32 n_colors;
     u32 n_important_colors;
+    u32 red_mask;
+    u32 green_mask;
+    u32 blue_mask;
+    u32 alpha_mask;
+    u32 color_space;
 };
 
 #pragma pack(pop)
+
+static bool
+is_bmp_compression_supported(const BMPCompressionType type) {
+    return type == BMPCompressionType::Bitfields;
+}
 
 [[nodiscard]] bool
 load_bmp(const mem::Allocator allocator, const Str filename, Image* out_image) {
     mem::Slice<u8> image_data = {};
     if (!io::read_file(allocator, filename, &image_data)) return false;
 
-    (void)out_image;
+    const BMPHeader* bmp_header = (BMPHeader*)image_data.ptr;
+    const DIBHeader* header = (DIBHeader*)(image_data.ptr + sizeof(BMPHeader));
 
-    return false;
+    const bool is_srgb = header->color_space == 'sRGB' || header->color_space == 'Win ';
+
+    if (!mem::equal(Str{ "BM" }, Str{ (u8*)bmp_header->ident, 2 }) || !is_srgb) {
+        fmt::print_stderr("Unsupported BMP version\n");
+        return false;
+    }
+
+    if (header->bpp != 32) {
+        fmt::print_stderr("Unsupported bpp format\n");
+        return false;
+    }
+
+    if (header->n_color_planes != 1) {
+        fmt::print_stderr("Number of color planes is not 1\n");
+        return false;
+    }
+
+    if (!is_bmp_compression_supported(header->compression)) {
+        fmt::print_stderr("Unsupported compression\n");
+        return false;
+    }
+
+    if (header->n_colors != 0) {
+        fmt::print_stderr("Color palettes unsupported\n");
+        return false;
+    }
+
+    const u32 stride = mem::align_up<u32>((header->bpp / 8) * (u32)header->width, 4);
+    u32* pixel_row = (u32*)(image_data.ptr + bmp_header->image_offset);
+
+    Image img = {
+        .width = (u32)header->width,
+        .height = (u32)header->height,
+        .bpp = header->bpp,
+    };
+    if (!allocator.alloc(stride * img.height, &img.pixels)) {
+        fmt::print_stderr("Failed to allocate memory\n");
+        return false;
+    }
+
+    for (u64 j = 0; j < img.height; ++j) {
+        for (u64 i = 0; i < img.width; ++i) {
+            const u32 pixel = pixel_row[i];
+            const u32 r = pixel & header->red_mask;
+            const u32 g = pixel & header->green_mask;
+            const u32 b = pixel & header->blue_mask;
+            const u32 a = pixel & header->alpha_mask;
+            const u32 out = (b << 24) | (g << 16) | (r << 8) | a;
+            (void)out;
+            img.pixels.ptr[j * img.width + i] = pixel;
+        }
+        pixel_row += stride;
+    }
+
+    *out_image = img;
+
+    return true;
 }
 
 } // namespace img
